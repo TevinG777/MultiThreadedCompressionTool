089e3f73bd0e751a80471eb0ae882ae8c820f1d5
TevinG777 <gajadhartevin@gmail.com>
2025-11-10 18:44:36 -0500

    template file

--- project2/src/serial.c
+++ project2/src/serial.c
@@ -1,183 +1,101 @@
-#include <dirent.h>
-#include <pthread.h>
-#include <stdio.h>
+#define SERIAL_IMPLEMENTATION
+
+#include "serial.h"
+
 #include <assert.h>
+#include <stdio.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 #include <zlib.h>
 
 #define BUFFER_SIZE 1048576 // 1MB
-#define MAX_WORKER_THREADS 19
 
-int cmp(const void *a, const void *b) {
-	return strcmp(*(char **) a, *(char **) b);
+static int cmp(const void *a, const void *b) {
+	return strcmp(*(char *const *) a, *(char *const *) b);
 }
 
-typedef struct {
-	const char *directory_name;
-	char **files;
-	int nfiles;
-	int next_index;
-	int next_write_index;
-	pthread_mutex_t index_lock;
-	pthread_mutex_t write_lock;
-	pthread_cond_t write_cond;
-	FILE *f_out;
-	long long total_in;
-	long long total_out;
-} compression_context_t;
+int list_txt_files(const char *dir, char ***out_files, int *out_n) {
+	(void)dir;
 
-typedef struct {
-	compression_context_t *ctx;
-	z_stream stream;
-	unsigned char *buffer_in;
-	unsigned char *buffer_out;
-} worker_state_t;
+	if(out_files != NULL)
+		*out_files = NULL;
+	if(out_n != NULL)
+		*out_n = 0;
 
-static void *compression_worker(void *arg) {
-	worker_state_t *worker = (worker_state_t *) arg;
-	compression_context_t *ctx = worker->ctx;
-	z_stream *strm = &worker->stream;
+	fprintf(stderr, "list_txt_files is a placeholder – implement me!\n");
+	return 0;
+}
 
-	memset(strm, 0, sizeof(z_stream));
-	int ret = deflateInit(strm, Z_BEST_COMPRESSION);
-	assert(ret == Z_OK);
+void init_context(compression_context_t *ctx, const char *dir, char **files, int n, FILE *f_out) {
+	assert(ctx != NULL);
+	ctx->directory = dir;
+	ctx->files = files;
+	ctx->file_count = n;
+	ctx->next_index = 0;
+	ctx->f_out = f_out;
+}
 
-	while(1) {
-		pthread_mutex_lock(&ctx->index_lock);
-		int idx = ctx->next_index++;
-		pthread_mutex_unlock(&ctx->index_lock);
+void destroy_context(compression_context_t *ctx) {
+	if(ctx == NULL)
+		return;
 
-		if(idx >= ctx->nfiles)
-			break;
-
-		size_t len = strlen(ctx->directory_name) + strlen(ctx->files[idx]) + 2;
-		char *full_path = malloc((len+1) * sizeof(char));
-		assert(full_path != NULL);
-		strcpy(full_path, ctx->directory_name);
-		strcat(full_path, "/");
-		strcat(full_path, ctx->files[idx]);
-
-		FILE *f_in = fopen(full_path, "rb");
-		assert(f_in != NULL);
-			int nbytes = fread(worker->buffer_in, sizeof(unsigned char), BUFFER_SIZE, f_in);
-		fclose(f_in);
-		free(full_path);
-
-		ret = deflateReset(strm);
-		assert(ret == Z_OK);
-		strm->avail_in = nbytes;
-		strm->next_in = worker->buffer_in;
-		strm->avail_out = BUFFER_SIZE;
-		strm->next_out = worker->buffer_out;
-
-		ret = deflate(strm, Z_FINISH);
-		assert(ret == Z_STREAM_END);
-
-		int nbytes_zipped = BUFFER_SIZE - strm->avail_out;
-
-		pthread_mutex_lock(&ctx->write_lock);
-		while(idx != ctx->next_write_index)
-			pthread_cond_wait(&ctx->write_cond, &ctx->write_lock);
-
-		fwrite(&nbytes_zipped, sizeof(int), 1, ctx->f_out);
-		fwrite(worker->buffer_out, sizeof(unsigned char), nbytes_zipped, ctx->f_out);
-		ctx->total_in += nbytes;
-		ctx->total_out += nbytes_zipped;
-		ctx->next_write_index++;
-		pthread_cond_broadcast(&ctx->write_cond);
-		pthread_mutex_unlock(&ctx->write_lock);
+	if(ctx->files != NULL) {
+		for(int i = 0; i < ctx->file_count; i++)
+			free(ctx->files[i]);
+		free(ctx->files);
 	}
 
-	deflateEnd(strm);
+	ctx->files = NULL;
+	ctx->file_count = 0;
+	ctx->next_index = 0;
+	ctx->f_out = NULL;
+}
+
+void *compression_worker(void *arg) {
+	(void)arg;
+	fprintf(stderr, "compression_worker is a placeholder – implement me!\n");
 	return NULL;
 }
 
+int spawn_workers(compression_context_t *ctx, int worker_count) {
+	(void)ctx;
+	(void)worker_count;
+	fprintf(stderr, "spawn_workers is a placeholder – implement me!\n");
+	return -1;
+}
+
 int compress_directory(char *directory_name) {
-	DIR *d;
-	struct dirent *dir;
-	char **files = NULL;
-	int nfiles = 0;
-
-	d = opendir(directory_name);
-	if(d == NULL) {
-		printf("An error has occurred\n");
-		return 0;
-	}
-
-	// create sorted list of text files
-	while ((dir = readdir(d)) != NULL) {
-		int len = strlen(dir->d_name);
-		if(len >= 4 && dir->d_name[len-4] == '.' && dir->d_name[len-3] == 't' && dir->d_name[len-2] == 'x' && dir->d_name[len-1] == 't') {
-			files = realloc(files, (nfiles+1)*sizeof(char *));
-			assert(files != NULL);
-
-			files[nfiles] = strdup(dir->d_name);
-			assert(files[nfiles] != NULL);
-
-			nfiles++;
-		}
-	}
-	closedir(d);
-	qsort(files, nfiles, sizeof(char *), cmp);
-
-	FILE *f_out = fopen("text.tzip", "wb");
-	assert(f_out != NULL);
-	setvbuf(f_out, NULL, _IOFBF, BUFFER_SIZE);
+	if(directory_name == NULL)
+		return -1;
 
 	compression_context_t ctx;
-	ctx.directory_name = directory_name;
-	ctx.files = files;
-	ctx.nfiles = nfiles;
-	ctx.next_index = 0;
-	ctx.next_write_index = 0;
-	ctx.f_out = f_out;
-	ctx.total_in = 0;
-	ctx.total_out = 0;
-	pthread_mutex_init(&ctx.index_lock, NULL);
-	pthread_mutex_init(&ctx.write_lock, NULL);
-	pthread_cond_init(&ctx.write_cond, NULL);
+	memset(&ctx, 0, sizeof(ctx));
 
-	if(nfiles > 0) {
-			int worker_count = nfiles < MAX_WORKER_THREADS ? nfiles : MAX_WORKER_THREADS;
-			if(worker_count == 0)
-				worker_count = 1;
+	if(list_txt_files(directory_name, &ctx.files, &ctx.file_count) != 0) {
+		fprintf(stderr, "list_txt_files failed.\n");
+		return -1;
+	}
 
-			pthread_t threads[MAX_WORKER_THREADS];
-			worker_state_t *worker_states = calloc(worker_count, sizeof(worker_state_t));
-			assert(worker_states != NULL);
+	FILE *f_out = fopen("text.tzip", "wb");
+	if(f_out == NULL) {
+		perror("text.tzip");
+		destroy_context(&ctx);
+		return -1;
+	}
 
-			for(int i=0; i<worker_count; i++) {
-				worker_states[i].ctx = &ctx;
-				worker_states[i].buffer_in = malloc(BUFFER_SIZE);
-				worker_states[i].buffer_out = malloc(BUFFER_SIZE);
-				assert(worker_states[i].buffer_in != NULL && worker_states[i].buffer_out != NULL);
-				int ret = pthread_create(&threads[i], NULL, compression_worker, &worker_states[i]);
-				assert(ret == 0);
-			}
-			for(int i=0; i<worker_count; i++) {
-				int ret = pthread_join(threads[i], NULL);
-				assert(ret == 0);
-				free(worker_states[i].buffer_in);
-				free(worker_states[i].buffer_out);
-			}
-			free(worker_states);
-		}
+	init_context(&ctx, directory_name, ctx.files, ctx.file_count, f_out);
 
-	pthread_mutex_destroy(&ctx.index_lock);
-	pthread_mutex_destroy(&ctx.write_lock);
-	pthread_cond_destroy(&ctx.write_cond);
+	int worker_status = spawn_workers(&ctx, 0);
+
 	fclose(f_out);
+	destroy_context(&ctx);
 
-	if(ctx.total_in == 0)
-		printf("Compression rate: 0.00%%\n");
-	else
-		printf("Compression rate: %.2lf%%\n", 100.0*(ctx.total_in-ctx.total_out)/ctx.total_in);
+	if(worker_status != 0) {
+		fprintf(stderr, "spawn_workers failed – the template is unfinished.\n");
+		return worker_status;
+	}
 
-	// release list of files
-	for(int i=0; i < nfiles; i++)
-		free(files[i]);
-	free(files);
-	// do not modify the main function after this point!
+	printf("Compression rate: 0.00%%\n");
 	return 0;
 }
--- src/serial.h
+++ src/serial.h
@@ -1,6 +1,24 @@
 #ifndef __SERIAL_H__
 #define __SERIAL_H__
 
+#include <stdio.h>
+
+typedef struct compression_context {
+	const char *directory;
+	char **files;
+	int file_count;
+	int next_index;
+	FILE *f_out;
+} compression_context_t;
+
 int compress_directory(char *directory_name);
+
+#ifdef SERIAL_IMPLEMENTATION
+int list_txt_files(const char *dir, char ***out_files, int *out_n);
+void init_context(compression_context_t *ctx, const char *dir, char **files, int n, FILE *f_out);
+void destroy_context(compression_context_t *ctx);
+void *compression_worker(void *arg);
+int spawn_workers(compression_context_t *ctx, int worker_count);
+#endif
 
 #endif
